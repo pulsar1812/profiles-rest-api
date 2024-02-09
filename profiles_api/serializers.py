5c49dd1c75e20c5d842c69929cecd5c658c51c48
pulsar1812 <cluster000@gmail.com>
2024-02-09 15:32:21 +0800

    Add profiles viewset

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,6 +1,33 @@
 from rest_framework import serializers
+from . import models
 
 
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing APIView"""
     name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """A serializer for the user profile object"""
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        """Creates and returns a new user"""
+        user = models.UserProfile(
+            email=validated_data['email'],
+            name=validated_data['name'],
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,8 +3,16 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from . import serializers
+from . import models
+from . import permissions
 
 
 class HelloApiView(APIView):
@@ -92,3 +100,14 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handles removing an object"""
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """ Handles listing, creating and updating profiles. """
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
